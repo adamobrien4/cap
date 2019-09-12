408497e765273b51a712c17a72c8bfdd9dbeabe4
Adam O'Brien <17226864@studentmail.ul.ie>
2019-09-12 21:11:21 +0100

    Initial Commit
    Added all work done as of now.

---
+++ Person.cpp
@@ -0,0 +1,6 @@
+#include "Person.h"
+
+Person::Person(int s_p, double m) {
+    start_position = s_p;
+    measurement = m;
+}
---
+++ Person.h
@@ -0,0 +1,8 @@
+class Person {
+    public:
+        double measurement;
+        int start_position = -10;
+        int cur_position = -10;
+
+        Person(int start_pos, double measurement);
+};
